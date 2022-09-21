e872da7b1940874bd0d65b86fb88e7a11787abd2
Suyoung Yun <87157566+Suyoung225@users.noreply.github.com>
2022-09-21 19:16:27 +0900

    삭제 기능 전까지 완성

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from pymongo import MongoClient
 import jwt
 import datetime
@@ -14,10 +16,8 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-
 client = MongoClient(config.dbaddress)
 db = client.dbsparta
-
 
 
 @app.route('/')
@@ -25,6 +25,8 @@ def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template("index.html", user_info=user_info)
         return render_template('index.html')
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
@@ -45,7 +47,7 @@ def sign_in():
     password_receive = request.form['password_give']
 
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one({'id': username_receive, 'pw': pw_hash})
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
 
     if result is not None:
         payload = {
@@ -66,40 +68,64 @@ def sign_up():
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "id": username_receive,
-        "pw": password_hash,
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        "profile_info": ""                                          # 프로필 한 마디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
-
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
-    exists = bool(db.users.find_one({"id": username_receive}))
+    exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('/sign_in/upload')
+def diarypage():
+    r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+    response = r.json()
+    rows = response['RealtimeCityAir']['row']
+    return render_template('upload.html', rows = rows)
+
+
+@app.route('/sign_in/upload', methods=['POST'])
 def posting():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"id": payload["id"]})
+        user_info = db.users.find_one({"username": payload["id"]})
+        username = payload["id"]
         text_receive = request.form["text_give"]
-        date_receive = request.form["date_give"]
+        # date_receive = request.form["date_give"]
+        today = datetime.now()
+        mytime = today.strftime("%Y-%m-%d-%H-%M-%S")
+        showtime = today.strftime("%Y/%m/%d %H:%M")
         weather_receive = request.form["weather_give"]
-        filepath_receive = request.form["filepath_give"]
         region_receive = request.form["region_give"]
 
+
+
+        file = request.files["file_give"]
+        filename = secure_filename(file.filename)
+        extension = filename.split(".")[-1]
+        file_path = f"{username}_{mytime}.{extension}"
+        file.save("./static/"+file_path)
         doc = {
-            "id": user_info["id"],
+            "username": user_info["username"],
+            "profile_name": user_info["profile_name"],
             "profile_pic_real": user_info["profile_pic_real"],
             "text": text_receive,
-            "date": date_receive,
+            "showtime" : showtime,
+            "date": mytime,
             "weather": weather_receive,
-            "filepath": filepath_receive,
-            "region": region_receive
+            "region": region_receive,
+            "file_name": filename,
+            "file_path": file_path
         }
         db.posts.insert_one(doc)
         return jsonify({"result": "success", 'msg': '포스팅 성공'})
@@ -112,18 +138,85 @@ def get_posts():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        gu_name_receive = request.args.get("gu_name")
+        username_receive = request.args.get("username_give")
+        if username_receive == "":
+            posts = list(db.posts.find({}).sort("date", -1).limit(20))
+        else:
+            posts = list(db.posts.find({"username": username_receive}).sort("date", -1).limit(20))
+        for post in posts:
+            post["_id"] = str(post["_id"])
+        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", 'posts': posts})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route("/get_posts_index", methods=['GET'])
+def get_posts_index():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        gu_name_receive = request.args.get("gu_name_give")
         if gu_name_receive == "":
             posts = list(db.posts.find({}).sort("date", -1).limit(20))
         else:
             posts = list(db.posts.find({"region": gu_name_receive}).sort("date", -1).limit(20))
-        posts = list(db.posts.find({}).sort("date", -1).limit(20))
         for post in posts:
             post["_id"] = str(post["_id"])
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", 'posts' : posts})
+        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", 'posts': posts})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
+
+@app.route('/user/<username>')
+def user(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('user.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+@app.route('/update_profile', methods=['POST'])
+def save_img():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username = payload["id"]
+        name_receive = request.form["name_give"]
+        about_receive = request.form["about_give"]
+        new_doc = {
+            "profile_name": name_receive,
+            "profile_info": about_receive
+        }
+        if 'file_give' in request.files:
+            file = request.files["file_give"]
+            filename = secure_filename(file.filename)
+            extension = filename.split(".")[-1]
+            file_path = f"profile_pics/{username}.{extension}"
+            file.save("./static/" + file_path)
+            new_doc["profile_pic"] = filename
+            new_doc["profile_pic_real"] = file_path
+        db.users.update_one({'username': payload['id']}, {'$set': new_doc})
+        return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/delete_post', methods=['POST'])
+def delete_post():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username = payload["id"]
+        showtime_receive = request.form["showtime_give"]
+        db.posts.delete_one({'showtime': showtime_receive})
+        return jsonify({"result": "success", 'msg': '삭제완료'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
