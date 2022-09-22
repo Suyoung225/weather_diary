9f5855c150362394f6b7b8cecc340da7267f57d9
Suyoung Yun <87157566+Suyoung225@users.noreply.github.com>
2022-09-22 15:45:12 +0900

    FINAL!!!!!

--- app.py
+++ app.py
@@ -20,6 +20,58 @@ SECRET_KEY = 'SPARTA'
 client = MongoClient(config.dbaddress)
 db = client.dbsparta
 
+seoul = ["강남구", "강동구", "강북구", "강서구", "관악구", "광진구", "구로구", "금천구", "노원구", "도봉구", "동대문구", "동작구", "마포구", "서대문구",
+            "서초구","성동구", "성북구", "송파구", "양천구", "영등포구", "용산구", "은평구", "종로구", "중구", "중랑구"]
+incheon = ["계양구", "남동구", "동구", "미추홀구", "부평구", "서구", "연수구", "중구"]
+gyeonggi = ["고양시", "과천시", "광명시", "광주시", "구리시", "군포시", "김포시", "남양주시", "동두천시", "부천시", "성남시", "수원시", "시흥시", "안산시",
+                    "안성시", "안양시", "양주시", "여주시", "오산시", "용인시", "의왕시", "의정부시", "이천시", "파주시", "평택시", "포천시", "하남시", "화성시",
+                    "가평군", "양평군", "연천군"]
+busan = ["강서구", "금정구", "남구", "동구", "동래구", "부산진구", "북구", "사상구", "사하구", "서구", "수영구", "연제구", "영도구", "중구", "해운대구"]
+daejeon = ["대덕구", "동구", "서구", "유성구", "중구"]
+daegu = ["남구", "달서구", "달서군", "동구", "북구", "서구", "수성구", "중구"]
+ulsan = ["남구", "동구", "북구", "중구", "울주군"]
+guwang = ["광산구", "남구", "동구", "북구", "서구"]
+gang = ["강릉시", "동해시", "삼척시", "속초시", "원주시", "춘천시", "태백시", "고성군", "양구군", "양양군", "영월군", "인제군", "정선군", "철원군", "평창군", "홍천군", "화천군", "횡성군"]
+chu_b = ["제천시", "청주시", "충주시", "괴산군", "단양군", "보은군", "영동군", "옥천군", "음성군", "증평군", "진천군"]
+chu_n = ["계룡시", "공주시", "논산시", "당진시", "보령시", "서산시", "아산시", "천안시", "금산군", "부여군", "서천군", "예산군", "청양군", "태안군", "홍성군"]
+gye_b = ["경산시", "경주시", "구미시", "김천시", "문경시", "상주시", "안동시", "영주시", "영천시", "포항시", "고령군", "군위군", "봉화군", "성주군", "영덕군", "영양군", "예천군", "울릉군", "울진군", "의성군", "청도군", "청송군", "칠곡군"]
+gye_n = ["거제시", "김해시", "밀양시", "사천시", "양산시", "진주시", "창원시", "통영시", "거창군", "고성군", "남해군", "산청군", "의령군", "창녕군", "하동군", "함안군", "함양군", "합천군"]
+jeo_b = ["군산시", "김제시", "남원시", "익산시", "전주시", "정읍시", "고창군", "무주군", "부안군", "순창군", "완주군", "임실군", "장수군", "진안군"]
+jeo_n = ["광양시", "나주시", "목포시", "순천시", "여수시", "강진군", "고흥군", "곡성군", "구례군", "담양군", "무안군", "보성군", "신안군", "영광군", "영암군", "완도군", "장성군", "장흥군", "진도군", "함평군", "해남군", "화순군"]
+
+region = list()
+for i in range(len(seoul)):
+    region.append("서울 " + seoul[i])
+for i in range(len(incheon)):
+    region.append("인천 " + incheon[i])
+for i in range(len(gyeonggi)):
+    region.append("경기 " + gyeonggi[i])
+for i in range(len(busan)):
+    region.append("부산 " + busan[i])
+for i in range(len(daejeon)):
+    region.append("대전 " + daejeon[i])
+for i in range(len(daegu)):
+    region.append("대구 " + daegu[i])
+for i in range(len(ulsan)):
+    region.append("울산 " + ulsan[i])
+for i in range(len(guwang)):
+    region.append("광주 " + guwang[i])
+region= region +["세종특별자치시", "제주 서귀포시", "제주 제주시"]
+for i in range(len(gang)):
+    region.append("강원 " + gang[i])
+for i in range(len(chu_b)):
+    region.append("충청북도 " + chu_b[i])
+for i in range(len(guwang)):
+    region.append("충청남도 " + chu_n[i])
+for i in range(len(gye_b)):
+    region.append("경상북도 " + gye_b[i])
+for i in range(len(gye_n)):
+    region.append("경상남도 " + gye_n[i])
+for i in range(len(jeo_b)):
+    region.append("전라북도 " + jeo_b[i])
+for i in range(len(jeo_n)):
+    region.append("전라남도 " + jeo_n[i])
+
 
 @app.route('/')
 def home():
@@ -27,7 +79,7 @@ def home():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.users.find_one({"username": payload["id"]})
-        return render_template("index.html", user_info=user_info)
+        return render_template("index.html", user_info=user_info, region = region)
         return render_template('index.html')
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
@@ -87,12 +139,13 @@ def check_dup():
 
 
 @app.route('/sign_in/upload')
+# def diarypage():
+#     r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+#     response = r.json()
+#     rows = response['RealtimeCityAir']['row']
+#     return render_template('upload.html', rows = rows)
 def diarypage():
-    r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-    response = r.json()
-    rows = response['RealtimeCityAir']['row']
-    return render_template('upload.html', rows = rows)
-
+    return render_template('upload.html', region = region)
 
 @app.route('/sign_in/upload', methods=['POST'])
 def posting():
@@ -143,7 +196,7 @@ def get_posts():
         if username_receive == "":
             posts = list(db.posts.find({}).sort("date", -1).limit(20))
         else:
-            posts = list(db.posts.find({"username": username_receive}).sort("date", -1).limit(20))
+            posts = list(db.posts.find({"username": username_receive}).sort("date", -1).limit(40))
         for post in posts:
             post["_id"] = str(post["_id"])
         return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", 'posts': posts})
@@ -159,7 +212,7 @@ def get_posts_index():
         if gu_name_receive == "":
             posts = list(db.posts.find({}).sort("date", -1).limit(20))
         else:
-            posts = list(db.posts.find({"region": gu_name_receive}).sort("date", -1).limit(20))
+            posts = list(db.posts.find({"region": gu_name_receive}).sort("date", -1).limit(40))
         for post in posts:
             post["_id"] = str(post["_id"])
         return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", 'posts': posts})
